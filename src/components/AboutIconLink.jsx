f627b55db559bef6a9b1802ff30c627fdfef619d
Sajid Hussain <sajidhussain98@gmail.com>
2022-01-01 02:19:21 +0530

    minor changes in About Icon Link component

--- src/components/AboutIconLink.jsx
+++ src/components/AboutIconLink.jsx
@@ -5,12 +5,7 @@ import { Link } from 'react-router-dom';
 function AboutIconLink() {
     return (
       <div className="about-link">
-        <Link to=
-        {
-            {
-                pathname:'/about'
-            }
-        }>
+        <Link to="/about">
           <FaQuestion size={30} />
         </Link>
       </div>
