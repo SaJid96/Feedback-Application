6564e1888159fc538b5e5b8f7bf3f75d3e23c945
Sajid Hussain <sajidhussain98@gmail.com>
2021-12-31 19:58:07 +0530

    added Feedback

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import {v4 as uuidv4} from 'uuid'
 import Header from "./components/Header";
 import FeedbackData from "./data/FeedbackData";
 import {useState} from 'react'
@@ -9,9 +10,16 @@ import FeedbackForm from "./components/FeedbackForm";
 function App() {
 
 
+
   const [feedback, setFeedback] = useState(FeedbackData)
 
 
+
+  const addedFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4();
+
+    setFeedback([newFeedback,...feedback]);
+  };
   const deleteFeedback=(id)=>{
 
       if (window.confirm('Are you sure yo want to delte?')) {
@@ -25,7 +33,7 @@ function App() {
       <div>
         <Header />
         <div className="container">
-          <FeedbackForm/>
+          <FeedbackForm handleAdd={addedFeedback} />
           <FeedbackStats feedback={feedback} />
           <FeedBackList feedback={feedback} handleDelete={deleteFeedback} />
         </div>
--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,55 +1,62 @@
-import { useState } from "react";
-import RatingSelect from "./RatingSelect";
-import Button from "./shared/Button";
-import Card from "./shared/Card"
-const FeedbackForm = () => {
+import { useState } from 'react';
+import RatingSelect from './RatingSelect';
+import Button from './shared/Button';
+import Card from './shared/Card';
+const FeedbackForm = ({ handleAdd }) => {
+  const [text, setText] = useState('');
+  const [rating, setRating] = useState(10);
 
-    const [text,setText]=useState('')
-    const [rating, setRating] = useState(10);
+  const [btndisabled, setBtndisabled] = useState(true);
+  const [message, setMessage] = useState('');
 
-    const [btndisabled, setBtndisabled] = useState(true);
-    const [message, setMessage] = useState('');
-
-    const handleText=(e)=>{
-
-
-        if (text === '') {
-            setBtndisabled(true)
-            setMessage(null)            
-        }
-        else if(text !== '' && text.trim().length<=10){
-            setBtndisabled(true)
-            setMessage('Text must be atleast 10 characters')
-        }
-        else{
-            setMessage(null)
-            setBtndisabled(false)
-        }
-
-        setText(e.target.value);
+  const handleText = (e) => {
+    if (text === '') {
+      setBtndisabled(true);
+      setMessage(null);
+    } else if (text !== '' && text.trim().length <= 10) {
+      setBtndisabled(true);
+      setMessage('Text must be atleast 10 characters');
+    } else {
+      setMessage(null);
+      setBtndisabled(false);
     }
 
-    return (
-      <Card>
-        <form>
-          <h2>How would you rate your service with us ?</h2>
-          {/* rating select component */}
-          <RatingSelect  select={(rating)=>setRating(rating)}/>
-          <div className="input-group">
-            <input
-              onChange={handleText}
-              type="text"
-              placeholder="write a review"
-              value={text}
-            />
-            <Button type="submit" isDisabled={btndisabled}>
-              Send
-            </Button>
-          </div>
-          {message && <div className="message">{message}</div>}
-        </form>
-      </Card>
-    );
-}
+    setText(e.target.value);
+  };
 
-export default FeedbackForm
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (text.trim().length > 10) {
+      const newFeedback = {
+        text,
+        rating,
+      };
+
+      handleAdd(newFeedback);
+      setText('');
+    }
+  };
+
+  return (
+    <Card>
+      <form onSubmit={handleSubmit}>
+        <h2>How would you rate your service with us ?</h2>
+        <RatingSelect select={(rating) => setRating(rating)} />
+        <div className="input-group">
+          <input
+            onChange={handleText}
+            type="text"
+            placeholder="write a review"
+            value={text}
+          />
+          <Button type="submit" isDisabled={btndisabled}>
+            Send
+          </Button>
+        </div>
+        {message && <div className="message">{message}</div>}
+      </form>
+    </Card>
+  );
+};
+
+export default FeedbackForm;
