5662169a044055b600abecf1e7d35b086ec034f8
Sajid Hussain <sajidhussain98@gmail.com>
2021-12-31 18:40:20 +0530

    Form validation

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -5,29 +5,51 @@ import Card from "./shared/Card"
 const FeedbackForm = () => {
 
     const [text,setText]=useState('')
+    const [btndisabled, setBtndisabled] = useState(true);
+    const [message, setMessage] = useState('');
 
     const handleText=(e)=>{
+
+
+        if (text === '') {
+            setBtndisabled(true)
+            setMessage(null)            
+        }
+        else if(text !== '' && text.trim().length<=10){
+            setBtndisabled(true)
+            setMessage('Text must be atleast 10 characters')
+        }
+        else{
+            setMessage(null)
+            setBtndisabled(false)
+        }
+
         setText(e.target.value);
     }
 
     return (
-    <Card>
+      <Card>
+        <form>
+          <h2>How would you rate your service with us ?</h2>
+          {/* rating select component */}
 
-<form>
- <h2>How would you rate your service with us ?</h2>
- {/* rating select component */}
+          <div className="input-group">
+            <input
+              onChange={handleText}
+              type="text"
+              placeholder="write a review"
+              value={text}
+            />
+            <Button type="submit" isDisabled={btndisabled}>
+              Send
+            </Button>
+          </div>
 
-<div className="input-group">
+            {message && <div className="message">{message}</div>}
 
-    <input onChange={handleText} type="text" placeholder="write a review" 
-    
-    value={text}
-    />
-    <Button type="submit">Send</Button>
-</div>
-</form>
-
-    </Card>);
+        </form>
+      </Card>
+    );
 }
 
 export default FeedbackForm
